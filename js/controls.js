3db2a2e2799757f9b255f1acb4cfe9c7d2ca06af
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-05 22:04:19 -0500

    make button looked press on key

--- js/controls.js
+++ js/controls.js
@@ -3,6 +3,15 @@
     return $('.js-'+key+'-button')
   };
 
+  var pressButton = function ($b) {
+    $b.addClass('active')
+    setTimeout(function () { $b.removeClass('active') }, 150)
+  }
+
+  var pressKey = function (key) {
+    pressButton($button(key))
+  }
+
   FriendHeads.checkRanges = function () {
     var opts = FriendHeads.options
     var rngs = FriendHeads.ranges
@@ -32,14 +41,22 @@
       var keyCode = e.which
       if(keyCode == 32) { // space bar
         FriendHeads.togglePaused()
+        pressKey('space')
       } else if (keyCode == 191) { // ? key
         FriendHeads.reverseHeads()
+        pressKey('question')
       } else if (keyCode == 38 || keyCode == 40) { // up arrow, down arrow
-        FriendHeads.changeSpeed(keyCode == 38)
+        var isUp = (keyCode == 38)
+        FriendHeads.changeSpeed(isUp)
+        isUp ? pressKey('up') : pressKey('down')
       } else if (keyCode == 187 || keyCode == 189 || keyCode == 173 || keyCode == 61) { // +-
-        FriendHeads.resizeHeads(keyCode == 187 || keyCode == 61)
+        var isPlus = (keyCode == 187 || keyCode == 61)
+        FriendHeads.resizeHeads(isPlus)
+        isPlus ? pressKey('plus') : pressKey('minus')
       } else if(keyCode == 37 || keyCode == 39) { // left arrow, right arrow
-        FriendHeads.changeSnail(keyCode==39)
+        var isRight = (keyCode==39)
+        FriendHeads.changeSnail(isRight)
+        isRight ? pressKey('right') : pressKey('left')
       } else {
         return
       }
