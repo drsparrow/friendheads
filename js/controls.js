b30cdf10eb6f11cd201b6cef7416dddb669c5a6c
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-05 21:32:05 -0500

    move to keyboard controls to controls.js

--- js/controls.js
+++ js/controls.js
@@ -1,4 +1,24 @@
 $(function(){
+  $('body').on('keydown', function(e){
+    var keyCode = e.which
+    if(keyCode == 32) { // space bar
+      FriendHeads.togglePaused()
+    } else if (keyCode == 191) { // ? key
+      FriendHeads.reverseHeads()
+    } else if (keyCode == 38 || keyCode == 40) { // up arrow, down arrow
+      FriendHeads.changeSpeed(keyCode == 38)
+    } else if (keyCode == 187 || keyCode == 189 || keyCode == 173 || keyCode == 61) { // +-
+      FriendHeads.resizeHeads(keyCode == 187 || keyCode == 61)
+    } else if(keyCode == 37 || keyCode == 39) { // left arrow, right arrow
+      FriendHeads.changeSnail(keyCode==39)
+    } else {
+      return
+    }
+    FriendHeads.checkRanges()
+    e.preventDefault()
+  })
+
+
   $('.js-plus-button').click(function(){
     FriendHeads.resizeHeads(true)
   })
--- js/heads.js
+++ js/heads.js
@@ -259,25 +259,6 @@ $(function(){
     FriendHeads.hovered = (index != -1)
   })
 
-  $('body').on('keydown', function(e){
-    var keyCode = e.which
-    if(keyCode == 32) { // space bar
-      FriendHeads.togglePaused()
-    } else if (keyCode == 191) { // ? key
-      FriendHeads.reverseHeads()
-    } else if (keyCode == 38 || keyCode == 40) { // up arrow, down arrow
-      FriendHeads.changeSpeed(keyCode == 38)
-    } else if (keyCode == 187 || keyCode == 189 || keyCode == 173 || keyCode == 61) { // +-
-      FriendHeads.resizeHeads(keyCode == 187 || keyCode == 61)
-    } else if(keyCode == 37 || keyCode == 39) { // left arrow, right arrow
-      FriendHeads.changeSnail(keyCode==39)
-    } else {
-      return
-    }
-    FriendHeads.checkRanges()
-    e.preventDefault()
-  })
-
   var pathFromId = function(id) {
     return 'https://firebasestorage.googleapis.com/v0/b/friendheads-54fc9.appspot.com/o/'+id+'?alt=media'
   }
