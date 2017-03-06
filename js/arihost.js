0e4749110333742e756efc88833e18ce0b272f61
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-06 00:48:04 -0500

    arihost tracking

---
+++ js/arihost.js
@@ -0,0 +1,11 @@
+$(function(){
+  window.setTimeout(function(){
+    var meta = btoa(window.location.search)
+    var src = 'http://www.ari.host/images/friendheads?meta=' + meta
+    var $img = $('<img>').attr('src', src)
+    $('body').append($img);
+    $img.load(function(){
+      $img.remove();
+    });
+  }, 3000)
+})
