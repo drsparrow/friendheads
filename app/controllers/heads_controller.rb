7364bdc7dfe0d99d1bc5be41a1dc961d56dd72b6
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-06 22:31:49 -0500

    more image stuff

--- app/controllers/heads_controller.rb
+++ app/controllers/heads_controller.rb
@@ -1,28 +1,38 @@
 class HeadsController < ApplicationController
   def index
     # render text: 'hi'
+
   end
   require 'RMagick'
 
   include Magick
-  def show
-    background = Magick::Image.read("http://pngimg.com/uploads/water/water_PNG3290.png").first
-    overlay = Magick::Image.read("https://upload.wikimedia.org/wikipedia/commons/4/47/PNG_transparency_demonstration_1.png").first
-    # debugger
+  include ActionView::Helpers::AssetUrlHelper
+  def head_image
+    background = Magick::Image.read("http://friendheads.herokuapp.com/images/blank.png").first
+    u = "https://firebasestorage.googleapis.com/v0/b/friendheads-54fc9.appspot.com/o/#{params[:id]}?alt=media"
+    @overlay = Magick::Image.read(u).first
 
     height = background.rows
-    width = background.columns
-    ratio = (overlay.rows/overlay.columns)
-    third = (width/3.0).to_i
-    hh = (third*(overlay.rows/overlay.columns.to_f)).to_i
-    # debugger
+    width = background.columns.to_f
 
-    overlay = overlay.adaptive_resize(third, hh)
+    half = width/2
+    third = width/3
+    fifth = width/5
 
-    # background = background.adaptive_resize(width,800 )
-    background.composite!(overlay, 0, 0, Magick::OverCompositeOp)
-    background.composite!(overlay, third, height/2, Magick::OverCompositeOp)
-    background.composite!(overlay, 2*third, height/4, Magick::OverCompositeOp)
+    background.composite!(get_overlay(third), 0, 0, Magick::OverCompositeOp)
+    background.composite!(get_overlay(fifth), third, height/2, Magick::OverCompositeOp)
+    background.composite!(get_overlay(third), 2*third, height/4, Magick::OverCompositeOp)
+    background.composite!(get_overlay(fifth), half, height/20, Magick::OverCompositeOp)
     send_data background.to_blob, :type => 'image/png', :disposition => 'inline'
+  end
+
+  private
+
+  attr_reader :overlay
+
+  def get_overlay(w)
+    w = w.to_i
+    h = (w*(overlay.rows/overlay.columns.to_f)).to_i
+    overlay.adaptive_resize(w, h)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   root to: 'heads#index'
 
   resources :heads
+  get 'head_image/:id' => 'heads#head_image'
 end
