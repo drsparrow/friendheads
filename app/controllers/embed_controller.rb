b1edff19806ec43575f49c2c7a7871ec214bf9f4
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-11 00:28:48 -0500

    update embed docs

--- app/controllers/embed_controller.rb
+++ app/controllers/embed_controller.rb
@@ -3,30 +3,45 @@ class EmbedController < ApplicationController
   def info
     @external_id = params[:id]
     @head = @external_id ? Head.find_by_external_id(@external_id) : Head.random_default
-    @both_script_tags = both_script_tags
-    @first_script_tag = first_script_tag
+    @easy_setup = easy_setup
+    @master_setup = master_setup
     @add_head_js = add_head_js
   end
 
   private
 
   def both_script_tags
-    min_js = add_head_after_wait.gsub(/\s+/, "")
+    min_js = add_head_after_wait_js.gsub(/\s+/, "")
 
     <<-HTML
       #{first_script_tag}<script>#{min_js}</script>
     HTML
   end
 
-  def first_script_tag
+  def setup_widget_js
+    <<-JAVASCRIPT
+      FriendheadsWidget.headId = '#{@head.external_id}';
+    JAVASCRIPT
+  end
+
+  def easy_setup
+    min_js = (setup_widget_js + add_head_after_wait_js).gsub(/\s+/, "")
     tag = <<-HTML
-      <script src="http://www.friendheads.com/js/embed.js"></script>
+      <script src="http://www.friendheads.com/js/embed.js"></script><script>#{min_js}</script>
     HTML
 
     tag.chomp
   end
 
-  def add_head_after_wait
+  def master_setup
+    tag = <<-HTML
+      <script src="http://www.friendheads.com/js/embed.js"></script><script>#{setup_widget_js.gsub(/\s+/, "")}</script>
+    HTML
+
+    tag.chomp
+  end
+
+  def add_head_after_wait_js
     <<-JAVASCRIPT
       document.addEventListener("DOMContentLoaded", function () {
         #{add_head_js}
@@ -36,7 +51,7 @@ class EmbedController < ApplicationController
 
   def add_head_js
     <<-JAVASCRIPT
-      FriendheadsWidget.add('#{@head.external_id}')
+      FriendheadsWidget.add()
     JAVASCRIPT
   end
 
