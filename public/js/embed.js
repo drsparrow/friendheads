2e3e8c35ef0515ac1e416aa220236522d7d71249
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-10 20:48:21 -0500

    add and remove heads in widget

--- public/js/embed.js
+++ public/js/embed.js
@@ -1,24 +1,55 @@
-window.FriendheadsWidget = function(id) {
-  var i = FriendheadsWidget.iframe = document.createElement('iframe');
-  i.src = 'https://friendheads.herokuapp.com/' + id + '?embeded=1';
-  i.setAttribute('tabindex', -1);
-  i.setAttribute('allowtransparency', true);
+;(function(){
 
-  var style = {
-    position: 'fixed',
-    top: '0px',
-    left: '0px',
-    width: '100vw',
-    height: '100vh',
-    overflow: 'hidden',
-    display: 'block',
-    border: 'none',
-    'pointer-events': 'none'
-  };
+  var widget = window.FriendheadsWidget = {};
+  FriendheadsWidget.iframes = {};
 
-  for(var key in style) {
-    i.style[key] = style[key];
+  widget.add = function(id) {
+    var iframe = document.createElement('iframe');
+
+    iframe.src = 'https://friendheads.herokuapp.com/' + id + '?embeded=1';
+    iframe.setAttribute('tabindex', -1);
+    iframe.setAttribute('allowtransparency', true);
+
+    var style = {
+      position: 'fixed',
+      top: '0px',
+      left: '0px',
+      width: '100vw',
+      height: '100vh',
+      overflow: 'hidden',
+      display: 'block',
+      border: 'none',
+      'pointer-events': 'none'
+    };
+
+    for(var key in style) {
+      iframe.style[key] = style[key];
+    }
+
+    document.body.append(iframe);
+    widget.iframes[id] = widget.iframes[id] || []
+    widget.iframes[id].push(iframe);
+
+    return iframe;
   }
 
-  document.body.append(i);
-}
+  widget.clear = function () {
+    for (var id in widget.iframes) {
+      widget.remove(id)
+    }
+
+    widget.iframes = {};
+  };
+
+  widget.remove = function (id) {
+    var iframes = widget.iframes[id];
+    if(!iframes) { return false }
+    iframes.forEach(function(iframe){
+      document.body.removeChild(iframe);
+    });
+
+    delete widget.iframes[id];
+    return true;
+  }
+
+})();
