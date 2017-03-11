08f2c9e861f5f845e4ede7f71bba89b51ad93932
drsparrow <abrenner@alumni.conncoll.edu>
2017-03-10 20:59:52 -0500

    load img over https

--- public/js/arihost.js
+++ public/js/arihost.js
@@ -1,7 +1,7 @@
 $(function(){
   window.setTimeout(function(){
     var meta = btoa(window.location.pathname+window.location.search)
-    var src = 'http://www.ari.host/images/friendheads?meta=' + meta
+    var src = 'https://ariuk.herokuapp.com/images/friendheads?meta=' + meta
     var $img = $('<img>').attr('src', src)
     $('body').append($img);
     $img.load(function(){
